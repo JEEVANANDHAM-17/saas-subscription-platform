95e4810a098bee58971a324b2a79d44da6741f9d
Jeevanandham A <jeevanandham170304@gmail.com>
2026-07-23 22:34:39 +0530

    Module name change

--- src/test/java/com/saas/subscription/SubscriptionApplicationTests.java
+++ src/test/java/com/saas/subscription/SubscriptionApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.jeevanandham.subscription;
+package com.saas.subscription;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApplicationTests {
+class SubscriptionApplicationTests {
 
 	@Test
 	void contextLoads() {
